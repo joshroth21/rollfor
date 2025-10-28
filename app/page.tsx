b538149e3d15d86d501dbf1d1e6dce9b6ba270d5
Josh Roth <josh@joshroth.me>
2025-10-28 10:15:17 -0700

    Add custom dice icon component & svgs

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { FaDiceD20 } from 'react-icons/fa';
+import { D20Icon } from "@/components/ui/d20-icon";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <main className="flex w-full max-w-3xl flex-col items-center justify-between">
         <h1 className="text-4xl font-semibold mb-1 flex items-center group">
-          <FaDiceD20 className='group-hover:animate-spin group-hover:scale-110 transition-all mr-2' />
+          <D20Icon className='group-hover:animate-spin group-hover:scale-110 transition-all mr-2 w-8' />
           <span>RollFor</span>
         </h1>
         <h2 className="text-lg italic font-mono opacity-50">
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import { FaDiceD20 } from 'react-icons/fa'
+import { D20Icon } from '../ui/d20-icon'
 
 export default function Header() {
   return (
 	<header className="bg-accent">
         <nav className="mx-auto container py-4 px-3 lg:py-3 mb-4">
           <div className="flex items-center lg:flex-1 lg:justify-center">
-            <FaDiceD20 className="mr-2" />
+            <D20Icon className="mr-2 w-5" />
             <span className="font-bold text-lg">RollFor.Me</span>
           </div>
         </nav>
