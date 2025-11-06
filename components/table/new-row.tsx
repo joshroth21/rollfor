7560915229bef4678b2e5f8df01cfed9465cce69
Josh Roth <josh@joshroth.me>
2025-11-05 18:20:58 -0800

    Enable light/dark mode in Storybook - Add dark mode to NewRow component

--- components/table/new-row.tsx
+++ components/table/new-row.tsx
@@ -9,7 +9,7 @@ type NewRowProps = {
 
 export default function NewRow({ label = 'New row' }: NewRowProps) {
   return (
-	<Button variant="outline" className="border-dashed shadow-none w-full text-gray-300 hover:text-gray-300 hover:bg-gray-50">
+	<Button variant="outline" className="border-dashed shadow-none w-full text-gray-300 dark:text-gray-600 hover:text-gray-300 dark:hover:text-gray-500 hover:bg-gray-50 dark:hover:bg-gray-800/50">
 		<Plus />
 		{label}
 	</Button>
